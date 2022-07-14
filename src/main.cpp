b73216f80ee9eb0e91f928d478fd8abb3d862d90
YuanJi Qiu <yuanji.qiu@usc.edu>
2022-07-13 17:38:43 -0700

    Project launched

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup() {
+    // put your setup code here, to run once:
+    log_e("App start!");
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
